8ae58b5a49f262b61b298ad2d5c2788b53bedb55
Albertomanas <amanas@cifpfbmoll.eu>
2020-08-08 22:21:38 +0200

    feat(Text): Método contarFrases

--- src/main/java/arteco/practica/domain/Text.java
+++ src/main/java/arteco/practica/domain/Text.java
@@ -6,28 +6,6 @@ import java.util.StringTokenizer;
 
 public class Text {
 
-    /*
-     Forma de hacerlo sin usar StringTokenizer.
-     */
-    /* public static Integer numeroPalabras(String lorem) {
-        int contador = 5, posicion;
-        //cont a 5 por los \n añadidos y por la última palabra
-        lorem = lorem.trim();
-        // eliminar posibles espacios en blanco del inicio y el final
-        if (lorem.isEmpty()) { //solo si la cadena está vacía
-            contador = 0;
-        } else {
-            posicion = lorem.indexOf(" ");
-            while (posicion != -1) {
-                contador ++;
-                posicion = lorem.indexOf(" ", posicion + 1);
-            }
-        }
-        return contador;
-    }
-
-
-     */
     //contador de palabras por linea
     public static Integer contarPalabras(String lorem) {
         Scanner sc = new Scanner(lorem);
@@ -40,5 +18,23 @@ public class Text {
         return contador;
     }
 
+    //contador de frases.
+    public static Integer contarFrases(String lorem) {
+        int contador = 0, posicion;
+        lorem = lorem.trim();
+        // eliminar posibles espacios en blanco del inicio y el final
+        if (lorem.isEmpty()) { //solo si la cadena está vacía
+            contador = 0;
+        } else {
+            posicion = lorem.indexOf(".");
+            while (posicion != -1) {
+                contador ++;
+                posicion = lorem.indexOf(".", posicion + 1);
+            }
+        }
+        return contador;
+    }
+
 
 }
+
