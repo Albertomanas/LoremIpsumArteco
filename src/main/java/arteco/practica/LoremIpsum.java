758f6ca1300a0f617ec2eaab9381a0c6ca325c64
Albertomanas <amanas@cifpfbmoll.eu>
2020-08-08 21:14:32 +0200

    feat(LoremIpsum): Siguiente historia de usuario

--- src/main/java/arteco/practica/LoremIpsum.java
+++ src/main/java/arteco/practica/LoremIpsum.java
@@ -1,12 +1,17 @@
 package arteco.practica;
 
 import arteco.practica.domain.Parser;
+import arteco.practica.domain.Text;
 
 public class LoremIpsum {
     public static void main(String[] args){
 
+        String lorem = Parser.parseLorem();
+
         System.out.println("\n#####" + "\t Operaciones requeridas: \t" + "#####\n");
 
-        System.out.println("5 primeros párrafos: \n" + Parser.parseLorem());
+        System.out.println("5 primeros párrafos: \n" + lorem);
+
+        System.out.println("El texto contiene: " + Text.numeroPalabras(lorem) + " palabras.");
     }
 }
