4cbbc0c0c7b53d4d30d8b505f17065258eeb0a81
Albertomanas <amanas@cifpfbmoll.eu>
2020-08-10 18:19:45 +0200

    feat(LoremIpsum): Historia de usuario de palindromos

--- src/main/java/arteco/practica/LoremIpsum.java
+++ src/main/java/arteco/practica/LoremIpsum.java
@@ -3,20 +3,29 @@ package arteco.practica;
 import arteco.practica.domain.Parser;
 import arteco.practica.domain.Text;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class LoremIpsum {
-    public static void main(String[] args){
+        public static void main(String[] args){
 
-        String lorem = Parser.parseLorem();
+            String lorem = Parser.parseLorem();
 
-        System.out.println("\n#####" + "\t Operaciones requeridas: \t" + "#####\n");
+            System.out.println("\n#####" + "\t Operaciones requeridas: \t" + "#####\n");
 
-        System.out.println("5 primeros párrafos: \n\n" + lorem + "\n");
+            System.out.println("5 primeros párrafos: \n\n" + lorem + "\n");
 
-        System.out.println("El texto contiene: " + Text.contarPalabras(lorem) + " palabras." + "\n");
+            System.out.println("El texto contiene: " + Text.contarPalabras(lorem) + " palabras." + "\n");
 
-        System.out.println("El texto contiene: " + Text.contarFrases(lorem) + " frases." + "\n");
+            System.out.println("El texto contiene: " + Text.contarFrases(lorem) + " frases." + "\n");
 
-        System.out.println("El texto contiene: " + Text.contarParrafos(lorem) + " parrafos." + "\n");
+            System.out.println("El texto contiene: " + Text.contarParrafos(lorem) + " parrafos." + "\n");
 
-    }
+            System.out.println("\n#####" + "\t Palindromos: \t" + "#####\n");
+
+            System.out.println("El texto contiene: " + Text.contarPalindromos(lorem).size() + " palindromos." + "\n");
+
+            System.out.println("Los cuales son: " + Text.contarPalindromos(lorem)+ "\n");
+
+        }
 }
