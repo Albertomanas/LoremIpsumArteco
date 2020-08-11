ad74553fd67ce04382856774f961331795f85c6f
Albertomanas <amanas@cifpfbmoll.eu>
2020-08-11 23:43:21 +0200

    feat(Text): Método que cuenta tuplas repetidas
    Se han añadido también comentarios

--- src/main/java/arteco/practica/domain/Text.java
+++ src/main/java/arteco/practica/domain/Text.java
@@ -37,16 +37,19 @@ public class Text {
         return contador;
     }
 
+
     //contador de párrafos
     public static Integer contarParrafos(String lorem) {
         int contador = 0;
         String saltoLinea = "\n";
-        while (lorem.indexOf(saltoLinea) > -1) {
+        // por defaul indexOf si no contiene nada es -1
+        while (lorem.indexOf(saltoLinea) != -1) {
             lorem = lorem.substring(lorem.indexOf(saltoLinea) + saltoLinea.length());
             contador++;
         }
         return contador;
     }
+
 
     //limpiar texto de signos de puntuación
     public static String quitarSignos(String lorem) {
@@ -87,12 +90,53 @@ public class Text {
         String texto = quitarSignos(lorem);
         StringTokenizer st = new StringTokenizer(texto);
 
+        /*
+            bucle que recorra toda la string.
+            Añade cada token y si encuentra otra igual, le suma al contador 1
+         */
         while (st.hasMoreTokens()){
             String palabras = st.nextToken();
+            // almacenar palabras dentro del diccionario, si esa palabra ya está aumenta el valor ans + 1
             filtrarPalabras.compute(palabras, (palabra, cantidad) -> (cantidad == null) ? 1 : cantidad +1);
         }
-        //LinkedMap para que salga ordenado
+        //LinkedMap para que salga ordenado capeado a 5
+        //Es necesario LinkedHashMap para que salga en orden descendiente
         LinkedHashMap mapaOrdenado = filtrarPalabras.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(5)
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (key, value) -> value,
+                        LinkedHashMap::new));
+        return mapaOrdenado;
+    }
+
+
+    public static Map<String, Integer> topTuplasRepetidas(String lorem) {
+        HashMap<String, Integer> tuplas = new HashMap<>();
+        String texto = quitarSignos(lorem);
+        StringTokenizer st = new StringTokenizer(texto);
+        String palabra = "";
+        String palabraAnterior = "";
+
+        while (st.hasMoreTokens()){
+            palabra = st.nextToken();
+            //si la palabra anterior contiene algo
+            if (palabraAnterior != "") {
+                //juntar strings para añadirlo al diccionario
+                String tuplaRepetida = palabraAnterior.concat(" ").concat(palabra);
+                if (tuplas.containsKey(tuplaRepetida)) {
+                    //incrementa el valor del mapa a 1
+                    tuplas.put(tuplaRepetida, tuplas.get(tuplaRepetida) + 1);
+                }else {
+                    //Almacena si no está almacenada ya la tupla
+                    tuplas.put(tuplaRepetida, 1);
+                }
+            }
+            palabraAnterior = palabra;
+        }
+
+        //LinkedMap para que salga ordenado capeado a 5
+        LinkedHashMap mapaOrdenado = tuplas.entrySet()
                 .stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .limit(5)
